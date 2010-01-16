e6075d4f8de41aebac58ab6bafb1f2f09df6d86f
James Alexander Shield <shemlesh@gmail.com>
2010-01-16 11:20:42 +0800

    Switched to DataMapper added Add This Support

---
+++ dist.settings.rb
@@ -0,0 +1,11 @@
+require 'ostruct'
+Blog = OpenStruct.new(
+    :title => 'a scanty blog',
+    :author => 'John Doe',
+    :url_base => 'http://localhost:4567/',
+    :admin_password => 'changeme',
+    :admin_cookie_key => 'scanty_admin',
+    :admin_cookie_value => '51d6d976913ace58',
+		:disqus_shortname => nil,
+		:add_this => nil
+)
